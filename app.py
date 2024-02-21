8dcdb6933c36bf5197e529e00086b1ba1d10c3bd
jongyoonkim <mw18074@alumni.bristol.ac.uk>
2024-02-22 01:27:14 +0900

    fine.

--- app.py
+++ app.py
@@ -38,33 +38,6 @@ VERIFICATION_TOKEN = os.getenv("VERIFICATION_TOKEN")
 ENCRYPT_KEY = os.getenv("ENCRYPT_KEY")
 LARK_HOST = os.getenv("LARK_HOST")
 
-# init service
-message_api_client = MessageApiClient(APP_ID, APP_SECRET, LARK_HOST)
-event_manager = EventManager()
-
-
-@event_manager.register("url_verification")
-def request_url_verify_handler(req_data: UrlVerificationEvent):
-    # url verification, just need return challenge
-    if req_data.event.token != VERIFICATION_TOKEN:
-        raise Exception("VERIFICATION_TOKEN is invalid")
-    return jsonify({"challenge": req_data.event.challenge})
-
-
-@event_manager.register("im.message.receive_v1")
-def message_receive_event_handler(req_data: MessageReceiveEvent):
-    sender_id = req_data.event.sender.sender_id
-    message = req_data.event.message
-    if message.message_type != "text":
-        app.logger.warning("Other types of messages have not been processed yet")
-        return jsonify()
-        # get open_id and text_content
-    open_id = sender_id.open_id
-    text_content = message.content
-    # echo text message
-    message_api_client.send_text_with_open_id(open_id, text_content)
-    return jsonify()
-
 
 @app.route("/", methods=['POST'])
 def main():
@@ -75,7 +48,6 @@ def main():
     response = ast.literal_eval(challenge)
 
     return jsonify(response)
-    # return event_handler(event)
 
 
 @app.route("/train", methods=['GET', 'POST'])
