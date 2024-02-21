e801ed43abcb10b39720882441013dc38bc1c1ac
jongyoonkim <mw18074@alumni.bristol.ac.uk>
2024-02-22 01:11:00 +0900

    bug fixed

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_apscheduler import APScheduler
 
 from src.train import Train, Running, Passenger
-from src.utils.event import EventManager
+from src.utils.api import MessageApiClient
+from src.utils.event import EventManager, UrlVerificationEvent, MessageReceiveEvent
 
 app = Flask(__name__)
 scheduler = APScheduler()
@@ -27,9 +28,39 @@ GROUP_ID = os.environ.get("GROUP_ID")
 running = []
 jobs = []
 
+# load from env
+APP_ID = os.getenv("APP_ID")
+APP_SECRET = os.getenv("APP_SECRET")
 VERIFICATION_TOKEN = os.getenv("VERIFICATION_TOKEN")
 ENCRYPT_KEY = os.getenv("ENCRYPT_KEY")
+LARK_HOST = os.getenv("LARK_HOST")
+
+# init service
+message_api_client = MessageApiClient(APP_ID, APP_SECRET, LARK_HOST)
 event_manager = EventManager()
+
+
+@event_manager.register("url_verification")
+def request_url_verify_handler(req_data: UrlVerificationEvent):
+    # url verification, just need return challenge
+    if req_data.event.token != VERIFICATION_TOKEN:
+        raise Exception("VERIFICATION_TOKEN is invalid")
+    return jsonify({"challenge": req_data.event.challenge})
+
+
+@event_manager.register("im.message.receive_v1")
+def message_receive_event_handler(req_data: MessageReceiveEvent):
+    sender_id = req_data.event.sender.sender_id
+    message = req_data.event.message
+    if message.message_type != "text":
+        app.logger.warning("Other types of messages have not been processed yet")
+        return jsonify()
+        # get open_id and text_content
+    open_id = sender_id.open_id
+    text_content = message.content
+    # echo text message
+    message_api_client.send_text_with_open_id(open_id, text_content)
+    return jsonify()
 
 
 @app.route("/", methods=['POST'])
