e41cd506594e3467eb585e0748e572b23c6b0de5
jongyoonkim <johnjongyoonkim@gmail.com>
2024-04-02 09:54:41 +0900

    [#8] auth header moved to authclient

--- src/lark/api/clients/message.py
+++ src/lark/api/clients/message.py
@@ -28,41 +28,38 @@ class MessageApiClient(AuthenticationApiClient):
         return objects
 
     def _send(self, receive_id_type: ReceiveIdType, receive_id, msg_type: MessageType, content):
-        self._authorize_tenant_access_token()
         send_objects = self._build_send_objects(
             url=self._lark_host / MESSAGE_URI,
             receive_id_type=receive_id_type,
             bodies=[{
                 "receive_id": receive_id,
                 "msg_type": msg_type.value,
-                "content": json.dumps(content)
+                "content": content
             }]
         )
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.tenant_access_token,
-        }
-        for send_object in send_objects:
-            send_object["headers"] = headers
+        send_objects = [{
+            **send_object,
+            "headers": self._get_auth_headers()
+        } for send_object in send_objects]
 
         responses = self._bulk_post_request(send_objects)
         assert len(responses) == 1
         return responses[0]
 
-    def send_open_id(self, open_id, msg_type: MessageType, content):
+    def _send_open_id(self, open_id, msg_type: MessageType, content):
         return self._send(ReceiveIdType.OPEN_ID, open_id, msg_type, content)
 
     def send_text(self, receive_id_type, receive_id, message):
-        return self._send(receive_id_type, receive_id, MessageType.TEXT, {"text": message})
+        return self._send(receive_id_type, receive_id, MessageType.TEXT, json.dumps({"text": message}))
 
     def send_text_with_open_id(self, open_id, message):
-        return self.send_open_id(open_id, MessageType.TEXT, {"text": message})
+        return self._send_open_id(open_id, MessageType.TEXT, json.dumps({"text": message}))
 
     def send_card(self, receive_id_type, receive_id, card_content: Dict[str, str]):
         return self._send(receive_id_type, receive_id, MessageType.INTERACTIVE, card_content)
 
     def send_card_with_open_id(self, open_id, card_content: Dict[str, str]):
-        return self.send_open_id(open_id, MessageType.INTERACTIVE, card_content)
+        return self._send_open_id(open_id, MessageType.INTERACTIVE, card_content)
 
     # ************* BULK SEND ************* #
     def bulk_send(self, receive_id_type: ReceiveIdType, receive_ids, msg_type: MessageType, content):
@@ -72,7 +69,7 @@ class MessageApiClient(AuthenticationApiClient):
             bodies=[{
                 "receive_id": receive_id,
                 "msg_type": msg_type.value,
-                "content": json.dumps(content)
+                "content": content
             } for receive_id in receive_ids]
         )
         return self._bulk_post_request(send_objects)
@@ -102,13 +99,10 @@ class MessageApiClient(AuthenticationApiClient):
                 "user_id_list": user_ids
             }]
         )
-
-        for buzz_object in buzz_objects:
-            headers = {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + self.tenant_access_token,
-            }
-            buzz_object["headers"] = headers
+        buzz_objects = [{
+            **send_object,
+            "headers": self._get_auth_headers()
+        } for send_object in buzz_objects]
 
         responses = self._bulk_post_request(buzz_objects)
         assert len(responses) == 1
@@ -136,12 +130,10 @@ class MessageApiClient(AuthenticationApiClient):
                 "content": content
             }]
         )
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + self.tenant_access_token,
-        }
-        for update_object in update_objects:
-            update_object["headers"] = headers
+        update_objects = [{
+            **send_object,
+            "headers": self._get_auth_headers()
+        } for send_object in update_objects]
 
         responses = self._bulk_patch_request(update_objects)
         assert len(responses) == 1
